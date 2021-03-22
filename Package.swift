643710cc0e1a4401588f7dc29b32433ebc18dc1e
Cimons <953330953@qq.com>
2021-03-22 16:47:45 +0800

    调整位置

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HBDNavigationBar",
-            dependencies: []),
+            dependencies: [],
+            path:"HBDNavigationBar"),
         .testTarget(
             name: "HBDNavigationBarTests",
             dependencies: ["HBDNavigationBar"]),
